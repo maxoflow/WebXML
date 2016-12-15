5e2f31cc46886018cfbfbdf78b372a2e326cd7d7
maxoflow <maxoflowinc@gmail.com>
2016-12-15 16:06:31 +0000

    up

---
+++ MaXoFloW-TV.php
@@ -0,0 +1,37 @@
+
+<html>
+<head>
+
+</head>
+
+<style>
+
+body {
+
+	margin:0px;
+
+	padding:0px;
+
+}
+
+</style>
+
+
+
+<body bgcolor="" style="padding: 0px 0px !important; text-align: LEFT;">
+
+<body>
+
+<title>MaXoFloW TV HD</title>
+
+<meta http-equiv="Content-Language" content="es" />
+<meta http-equiv="?" content="no-cache">
+<meta http-equiv="Expires" content="0">
+
+<iframe src='//app.viloud.tv/player/embed/channel/931b15369c39acb16b1853cc7462bec2?autoplay=0&volume=1&random=0&controls=1&title=1&share=0' width='300' height='200' frameborder='0' allowfullscreen></iframe>
+
+<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" allowfullscreen width="650" height="400" src="http://cinestrenostv.tv/canales/envivo/axn.html"></iframe>
+
+
+</body>
+</html>
