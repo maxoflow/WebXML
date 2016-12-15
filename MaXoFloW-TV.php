7fb7a8655bf0a508665260ccef0b546bd3826dde
maxoflow <maxoflowinc@gmail.com>
2016-12-15 16:31:21 +0000

    up

--- MaXoFloW-TV.php
+++ MaXoFloW-TV.php
@@ -28,9 +28,8 @@ body {
 <meta http-equiv="?" content="no-cache">
 <meta http-equiv="Expires" content="0">
 
-<iframe src='//app.viloud.tv/player/embed/channel/931b15369c39acb16b1853cc7462bec2?autoplay=0&volume=1&random=0&controls=1&title=1&share=0' width='300' height='200' frameborder='0' allowfullscreen></iframe>
+<iframe src='//app.viloud.tv/player/embed/channel/931b15369c39acb16b1853cc7462bec2/video/5b0c5b717c12ab03ea0383a3f5d3d521?autoplay=1&volume=1&random=0&controls=1&title=1&share=1' width='650' height='400' frameborder='0' allowfullscreen></iframe>
 
-<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" allowfullscreen width="650" height="400" src="http://cinestrenostv.tv/canales/envivo/axn.html"></iframe>
 
 
 </body>
